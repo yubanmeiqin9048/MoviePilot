9ed7db30076d6df94797642c18c918b984f7cfd0
yubanmeiqin9048 <64676973+yubanmeiqin9048@users.noreply.github.com>
2024-01-20 14:50:11 +0800

    Update spider.py

--- app/modules/indexer/spider.py
+++ app/modules/indexer/spider.py
@@ -89,6 +89,23 @@ class TorrentSpider:
         self.category = indexer.get('category')
         self.list = indexer.get('torrents').get('list', {})
         self.fields = indexer.get('torrents').get('fields')
+        if str(self.indexername).lower() == "mikan":
+            if keyword:
+                self.fields['title'] = {'selector': 'td:nth-child(1)'}
+                self.fields['date_added'] = {'selector': 'td:nth-child(3)'}
+                self.fields['download'] = {'selector': 'td:nth-child(4) > a', 'attribute': 'href'}
+            else:
+                self.list['selector'] = 'table.table.table-striped.tbl-border.fadeIn > tbody > tr'
+                self.fields['id'] = {'selector':'tr'}
+                self.fields['title'] = {'selector': 'td:nth-child(3) > a.magnet-link-wrap'}
+                self.fields['detail'] = {'selector': 'td:nth-child(3) > a.magnet-link-wrap', 'attribute': 'href'}
+                self.fields['download'] = {'selector': 'td:nth-child(5) > a', 'attribute': 'href'}
+                self.fields['date_added'] = {'selector': 'td:nth-child(1)'}
+                self.fields['size'] = {'selector': 'td:nth-child(4)'}
+        elif str(self.indexername) == '猫站':
+            self.fields['title'] = {'selector': 'td:nth-child(2) div div > a > b'}
+            self.fields['description'] = {'selector': 'td:nth-child(2) div div > span'}
+            self.fields['labels'] = {'selector': 'td:nth-child(2) div div > a.torrents-tag'} 
         self.render = indexer.get('render')
         self.domain = indexer.get('domain')
         self.page = page
